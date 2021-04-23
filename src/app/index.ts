2cfd718749cac43810e4a9f607f6e51aed0fc81a
Sellakumar <sellakumar.krishnamoorthi@syncfusion.com>
2021-04-23 14:44:45 +0530

    project changes committed

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,65 +1,78 @@
-import { Ajax, L10n, loadCldr, setCulture, enableRtl } from '@syncfusion/ej2-base';
-import { Month, Schedule, Week, DragAndDrop } from '@syncfusion/ej2-schedule';
+import { Schedule, Day, Week, WorkWeek, Month, Agenda, MonthAgenda, Print } from '@syncfusion/ej2-schedule';
 
-Schedule.Inject(Week, Month, DragAndDrop);
-
-function loadCultureFiles(name: string, base: boolean = false): void {
-    let files: string[] = !base ?
-        ['ca-gregorian.json', 'numbers.json', 'timeZoneNames.json', 'currencies.json', 'ca-islamic.json'] : ['numberingSystems.json'];
-    for (let prop of files) {
-        let val: Object;
-        let ajax: Ajax;
-        if (base) {
-            ajax = new Ajax('./cldr-data/supplemental/' + prop, 'GET', false);
-        } else {
-            ajax = new Ajax('./cldr-data/main/' + name + '/' + prop, 'GET', false);
-        }
-        ajax.onSuccess = (value: JSON) => {
-            val = value;
-        };
-        ajax.send();
-        loadCldr(JSON.parse(<string>val));
-    }
-}
-
-loadCultureFiles('en', true);
-loadCultureFiles('ar');
-loadCultureFiles('de');
-loadCultureFiles('fr-CH');
-loadCultureFiles('zh');
-
-let ajax: Ajax = new Ajax('./cldr-data/locale.json', 'GET', false);
-ajax.onSuccess = (value: string) => { L10n.load(JSON.parse(value)); };
-ajax.send();
-
-setCulture('de');
+Schedule.Inject(Day, Week, WorkWeek, Month, Agenda, MonthAgenda, Print);
 
 let scheduleObj = new Schedule({
+    currentView: 'Agenda',
     width: '100%', height: '555px',
-    views: ['Week', 'Month'],
-    selectedDate: new Date(2021, 0, 5),
+    selectedDate: new Date(2017, 10, 1),
     eventSettings: {
-        dataSource: [
-            {
-                Id: 1,
-                Subject: 'Recurrence',
-                StartTime: new Date(2021, 0, 4, 9),
-                EndTime: new Date(2021, 0, 4, 14, 30),
-                RecurrenceRule: 'FREQ=DAILY;INTERVAL=1;COUNT=5',
-                IsAllDay: false
-            }
-        ]
+        dataSource: generateObject()
     }
 });
 scheduleObj.appendTo('#schedule');
 
-document.getElementById('timeFormat').onchange = (args): void => {
-    scheduleObj.timeFormat = (args.target as HTMLSelectElement).value;
+document.getElementById('print').onclick = (): void => {
+    scheduleObj.print({
+        height: (document.getElementById('printHeight') as HTMLSelectElement).value,
+        width: (document.getElementById('printWidth') as HTMLSelectElement).value,
+        printView: scheduleObj.currentView
+    });
 };
 
-document.getElementById('cultures').onchange = (e) => {
-    let ddl: HTMLSelectElement = document.getElementById('cultures') as HTMLSelectElement;
-    let value: string = ddl.value;
-    enableRtl(value === 'ar');
-    setCulture(value);
-};
+function generateObject(): Record<string, any>[] {
+    const data: Record<string, any>[] = [];
+    const names: string[] = [
+        'Bering Sea Gold', 'Technology', 'Maintenance', 'Meeting', 'Travelling', 'Annual Conference', 'Birthday Celebration',
+        'Farewell Celebration', 'Wedding Anniversary', 'Alaska: The Last Frontier', 'Deadest Catch', 'Sports Day',
+        'MoonShiners', 'Close Encounters', 'HighWay Thru Hell', 'Daily Planet', 'Cash Cab', 'Basketball Practice',
+        'Rugby Match', 'Guitar Class', 'Music Lessons', 'Doctor checkup', 'Brazil - Mexico', 'Opening ceremony', 'Final presentation'
+    ];
+    const start: number = new Date(2017, 0, 1).getTime();
+    const end: number = new Date(2018, 11, 31).getTime();
+    const dayCount: number = 1000 * 60 * 60;
+    for (let a: number = start, id: number = 3; a < end; a += (dayCount * 24) * 2) {
+        const count: number = Math.floor((Math.random() * 9) + 1);
+        for (let b: number = 0; b < count; b++) {
+            const hour: number = Math.floor(Math.random() * 100) % 24;
+            const minutes: number = Math.round((Math.floor(Math.random() * 100) % 60) / 5) * 5;
+            const nCount: number = Math.floor(Math.random() * names.length);
+            const startDate: Date = new Date(new Date(a).setHours(hour, minutes));
+            const endDate: Date = new Date(startDate.getTime() + (dayCount * 2.5));
+            data.push({
+                Id: id,
+                Subject: names[nCount],
+                StartTime: startDate,
+                EndTime: endDate,
+                IsAllDay: (id % 10) ? false : true
+            });
+            id++;
+        }
+    }
+    const longerEvent: Record<string, any> = {
+        Id: 0,
+        StartTime: new Date(2017, 0, 1),
+        EndTime: new Date(2017, 0, 10),
+        IsAllDay: true,
+        Location: 'Chennai'
+    };
+    const occurrenceEvent: Record<string, any> = {
+        Id: 1,
+        StartTime: new Date(2017, 0, 1),
+        EndTime: new Date(2017, 0, 10),
+        IsAllDay: true,
+        RecurrenceRule: 'FREQ=DAILY;INTERVAL=1;COUNT=5',
+        RecurrenceId: 0
+    };
+    const recurrenceEvent: Record<string, any> = {
+        Id: 2,
+        StartTime: new Date(2017, 0, 1),
+        EndTime: new Date(2017, 0, 10),
+        IsAllDay: true,
+        RecurrenceRule: 'FREQ=DAILY;INTERVAL=1'
+    };
+    data.push(longerEvent);
+    data.push(occurrenceEvent);
+    data.push(recurrenceEvent);
+    return data;
+}
